1768ce9323dcca8ddffaec4b0a17fa41104daded
AntohaSimonoha <73301782+AntohaSimonoha@users.noreply.github.com>
2021-10-03 19:21:46 +0300

    Update BPW34_w_PDRES.ino

--- BPW34_w_PDRES/BPW34_w_PDRES.ino
+++ BPW34_w_PDRES/BPW34_w_PDRES.ino
@@ -2,24 +2,21 @@ void setup() {
   // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
 }
-
 void loop() {
   // Get a reading from the photodiode
   int reading = analogRead(A6);
-
   // Convert the reading into voltage:
   float voltage = reading * (5000 / 1024.0);
-
+  
   float lux = 1.33333 * voltage;
 
   // Print reading in the Serial Monitor
   Serial.println();
-  Serial.println(reading);
-  Serial.print(voltage);
+  Serial.println(reading); // print reading
+  Serial.print(voltage);   // print voltage
   Serial.println(" mV");
-  Serial.print(lux);
+  Serial.print(lux);       // print light illuminance
   Serial.println(" lux");
 
   delay(1000); // wait one second between readings
-
 }
