bbd3136a5d1c4a0e61ae1fe8fbb2d273895e0e8c
AntohaSimonoha <73301782+AntohaSimonoha@users.noreply.github.com>
2021-10-03 19:26:52 +0300

    Update BPW34_SIMPLE.ino

--- BPW34_SIMPLE/BPW34_SIMPLE.ino
+++ BPW34_SIMPLE/BPW34_SIMPLE.ino
@@ -2,11 +2,9 @@ void setup() {
   // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
 }
-
 void loop() {
   // Get a reading from the photodiode
   int reading = analogRead(A6);
-
   // Convert the reading into voltage:
   float voltage = reading * (5000 / 1024.0);
 
