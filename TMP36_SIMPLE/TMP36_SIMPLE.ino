313d15314e1da3f122e4431c59720bd1858c05e6
AntohaSimonoha <73301782+AntohaSimonoha@users.noreply.github.com>
2021-10-03 19:04:50 +0300

    Update TMP36_SIMPLE.ino

--- TMP36_SIMPLE/TMP36_SIMPLE.ino
+++ TMP36_SIMPLE/TMP36_SIMPLE.ino
@@ -1,21 +1,16 @@
 /* Used the code from the link below as a base */
 /* TMP36 analog temperature sensor with Arduino example code. More info: https://www.makerguides.com */
-
 void setup() {
   // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
 }
-
 void loop() {
   // Get a reading from the temperature sensor:
   int reading = analogRead(A6); 
-
   // Convert the reading into voltage:
   float voltage = reading * (5000 / 1024.0);
-
   // Convert the voltage into the temperature in Celsius:
   float temperature = (voltage - 500) / 10; 
-
   // Print reading, voltage and temperature in the Serial Monitor:
   Serial.println();
   Serial.println(reading);
@@ -24,6 +19,6 @@ void loop() {
   Serial.print(temperature);
   Serial.print(" \xC2\xB0"); // degree symbol
   Serial.println("C");
-
-  delay(1000); // wait a second between readings
+  
+  delay(1000); // wait one second between readings
 }
